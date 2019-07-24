f6ec2f2af90d49f6e32ebc745b374b19619274a8
agile.zhou <kklldog@live.cn>
2019-07-24 23:23:46 +0800

    添加项目文件。

---
+++ PublishHook/Logger.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Text;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Agile.FrameworkNetCore.Log
+{
+    public static class Logger
+    {
+        #region Trace
+
+        public static void Trace(string message)
+        {
+            Trace(message, null);
+        }
+
+        public static void Trace(string message, Type sender)
+        {
+            Trace(message, sender, null);
+        }
+
+        public static void Trace(string message, Type sender, Exception ex)
+        {
+            WriteLog(message, sender, ex, LogType.Trace);
+        }
+
+        #endregion
+
+        #region Debug
+
+        public static void Debug(string message)
+        {
+            Debug(message, null);
+        }
+
+        public static void Debug(string message, Type sender)
+        {
+            Debug(message, sender, null);
+        }
+
+        public static void Debug(string message, Type sender, Exception ex)
+        {
+            WriteLog(message, sender, ex, LogType.Debug);
+        }
+
+        #endregion
+
+        #region Info
+
+        public static void Info(string message)
+        {
+            Info(message, null);
+        }
+
+        public static void Info(string message, Type sender)
+        {
+            Info(message, sender, null);
+        }
+
+        public static void Info(string message, Type sender, Exception ex)
+        {
+            WriteLog(message, sender, ex, LogType.Info);
+        }
+
+        #endregion
+
+        #region Warning
+
+        public static void Warning(string message, Type sender)
+        {
+            Warning(message, sender, null);
+        }
+
+        public static void Warning(string message, Type sender, Exception ex)
+        {
+            WriteLog(message, sender, ex, LogType.Warning);
+        }
+
+        #endregion
+
+        #region Error
+
+        public static void Error(string message, Type sender)
+        {
+            Error(message, sender, null);
+        }
+
+        public static void Error(string message, Type sender, Exception ex)
+        {
+            WriteLog(message, sender, ex, LogType.Error);
+        }
+
+        #endregion
+
+        #region Fatal
+
+        public static void Fatal(string message, Type sender)
+        {
+            Fatal(message, sender, null);
+        }
+
+        public static void Fatal(string message, Type sender, Exception ex)
+        {
+            WriteLog(message, sender, ex, LogType.Fatal);
+        }
+
+        #endregion
+
+        #region Private
+
+        private static void WriteLog(string message, Type sender, Exception exception, LogType type)
+        {
+            string logMessage = GenerateMessage(message, type, sender, exception);
+
+            LogEventInfo logEvent = new LogEventInfo { LoggerName = sender == null ? "System" : sender.FullName, TimeStamp = DateTime.Now };
+            logEvent.Properties.Add("LogMessage", logMessage);
+            logEvent.Properties.Add("AppLocation", _appLocation);
+
+            switch (type)
+            {
+                case LogType.Trace:
+                    logEvent.Level = LogLevel.Trace;
+                    break;
+                case LogType.Debug:
+                    logEvent.Level = LogLevel.Debug;
+                    break;
+                case LogType.Info:
+                    logEvent.Level = LogLevel.Info;
+                    break;
+                case LogType.Warning:
+                    logEvent.Level = LogLevel.Warn;
+                    break;
+                case LogType.Error:
+                    logEvent.Level = LogLevel.Error;
+                    break;
+                case LogType.Fatal:
+                    logEvent.Level = LogLevel.Fatal;
+                    break;
+            }
+
+            _logger.Log(logEvent);
+#if DEBUG
+            System.Diagnostics.Debug.WriteLine(logMessage);
+#endif
+        }
+
+        private static string GenerateMessage(string message, LogType type, Type sender, Exception ex)
+        {
+            return string.Format("{0} {1} {2}\r\n{3}\r\n",
+                                 DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.mmm"),
+                                 type.ToString().ToUpper(),
+                                 sender == null ? "System" : sender.FullName,
+                                 ex == null
+                                     ? "Message : " + message
+                                     : string.Format("Message : {0}\r\nException : {1}\r\nStackTrace :\r\n{2}", message, ex.Message, ex.StackTrace));
+        }
+
+        private static void ConfigLog()
+        {
+            LoggingConfiguration config = new LoggingConfiguration();
+
+            FileTarget fileTarget = new FileTarget
+            {
+                Encoding = Encoding.UTF8,
+                FileName = @"${event-context:item=AppLocation}\\logs\\${shortdate}\\[${level}]${logger}.txt",
+                Layout = @"${event-context:item=LogMessage}"
+            };
+            LoggingRule rule = new LoggingRule("*", LogLevel.Trace, fileTarget);
+            config.AddTarget("file", fileTarget);
+            config.LoggingRules.Add(rule);
+
+            FileTarget timelineTarget = new FileTarget
+            {
+                Encoding = Encoding.UTF8,
+                FileName = @"${event-context:item=AppLocation}\\logs\\${shortdate}\\Timeline.txt",
+                Layout = @"${event-context:item=LogMessage}"
+            };
+            LoggingRule timelineRule = new LoggingRule("*", LogLevel.Trace, timelineTarget);
+            config.AddTarget("timeline", timelineTarget);
+            config.LoggingRules.Add(timelineRule);
+
+            LogManager.Configuration = config;
+        }
+
+        static Logger()
+        {
+            _appLocation = AppDomain.CurrentDomain.BaseDirectory;
+            ConfigLog();
+        }
+
+        private enum LogType { Trace, Debug, Info, Warning, Error, Fatal }
+        private static readonly NLog.Logger _logger = LogManager.GetCurrentClassLogger();
+        private static readonly string _appLocation;
+
+        #endregion
+    }
+}
---
+++ PublishHook/Program.cs
@@ -0,0 +1,64 @@
+﻿using Agile.FrameworkNetCore.Log;
+using System;
+using System.Diagnostics;
+
+namespace PublishHook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PublishHook is running now !");
+
+            var server = new Agile.AServer.Server();
+            server.AddHandler(new Agile.AServer.HttpHandler()
+            {
+                Method = "POST",
+                Path = "/api/hook",
+                Handler = (req, resp) =>
+                {
+                    string shell_name = req.Query.shell;
+                    if (!string.IsNullOrEmpty(shell_name))
+                    {
+                        RunShell(shell_name);
+                    }
+                    return resp.Write("ok");
+                }
+            });
+
+            server
+                .SetIP("0.0.0.0")
+                .SetPort(9000)
+                .Run();
+
+            Console.Read();
+        }
+
+        static void RunShell(string fileName)
+        {
+            var processStartInfo = new ProcessStartInfo(fileName) { RedirectStandardOutput = true };
+            var process = Process.Start(processStartInfo);
+            if (process == null)
+            {
+                Console.WriteLine("Can not run shell .");
+            }
+            else
+            {
+                using (var sr = process.StandardOutput)
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var str = sr.ReadLine();
+                        Console.WriteLine(str);
+                        Logger.Info(str);
+                    }
+
+                    if (!process.HasExited)
+                    {
+                        process.Kill();
+                    }
+                }
+            }
+        }
+    }
+}
